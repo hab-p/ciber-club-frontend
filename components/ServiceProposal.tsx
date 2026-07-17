b4548ce7da8c85c5286e314f46721e15020f7e3c
Henry Junior Alvarez Bustamante <halvarez@mimstechcorp.com>
2026-07-17 17:15:10 -0300

    feat: Add service proposal link update

--- components/ServiceProposal.tsx
+++ components/ServiceProposal.tsx
@@ -12,7 +12,7 @@ export default function ServiceProposal() {
         
         <div className="w-full bg-gray-800 rounded-xl shadow-lg border border-gray-700 overflow-hidden p-8 flex justify-center">
              <iframe 
-               src="https://drive.google.com/file/d/1PYcl0y2NxTZ02LlTO3oHgjhZutsQXbMI/preview" 
+               src="https://drive.google.com/file/d/1qZbr2vHERh5DasFIgrBxlYX9tFNJHiYV/preview" 
                className="w-full h-[800px] rounded-lg"
                title="Propuesta Servicio para Eventos públicos y privados (2026)"
              ></iframe>
